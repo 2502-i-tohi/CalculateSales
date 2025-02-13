e4196bf44ca2656551019521444b5f25a8162a04
i_tohi <i.tohi@alhinc.jp>
2025-02-13 16:58:43 +0900

    更新

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -89,7 +89,7 @@ public class CalculateSales {
 					return;
 				}
 				
-				if(!salesList.get(1).matches("^[0-9]$")) {
+				if(!salesList.get(1).matches("^[0-9]*$")) {
 					System.out.println(UNKNOWN_ERROR);
 					return;
 				}
